d7cc58c9d36d8c95ac7c9c1531ca9002bdb600a6
KoteTop8414 <93428046+KoteTop8414@users.noreply.github.com>
2022-10-20 18:21:24 +0300

    Update meowmrrr.py

--- meowmrrr.py
+++ meowmrrr.py
@@ -1,12 +1,4 @@
-def filework(filename , opentype):
-    if opentype == 'w':
-        f = open(filename, 'w')
-        filecontent = input('content~')
-        f.write(filecontent)
-        f.close()
-    if opentype == 'r':
-        f = open(filename, 'r')
-        print(f.read())
+from tkinter import *
 def wait(timewait, waittext):
     import time
     print(waittext + str(timewait))
@@ -14,14 +6,8 @@ def wait(timewait, waittext):
 def close(closelog):
     import sys
     sys.exit('programm closed, reason: ' + closelog)
-
-class cmd:
-    def log(text):
-        print(text)
-    def close(closelog):
-        import sys
-        sys.exit('programm closed, reason: ' + closelog)
-console = cmd
+def log(text):
+    print(text)
 
 class f:
     def read(filename):
@@ -34,6 +20,31 @@ class f:
         fileopen.close()
 
 filee = f
+
+class message:
+    def info(contents, messages):
+        from tkinter import messagebox
+        messagebox.showinfo(contents, message=messages)
+    def warning(contents, messages):
+        from tkinter import messagebox
+        messagebox.showwarning(contents, message=messages)
+    def error(contents, messages):
+        from tkinter import messagebox
+        messagebox.showerror(contents, message=messages)
+class welcome:
+    def default(programm_name, message, contents):
+        from tkinter import messagebox
+        messagebox.showinfo(contents, message='welcome to the ' + programm_name + ', ' + message)
+    def interface(programm_name, message, contents, size, color, fontsize):
+        root = Tk()
+        root.title(contents)
+        root.geometry(size)
+        root['bg'] = color
+        info = 'welcome to the ' + programm_name + ', ' + message
+        Label(root, text=info, bg=color, font="Arial " + fontsize).pack()
+        Button(root, text='close', bg=color, font="Arial " + fontsize, command=lambda: root.destroy()).pack()
+        root.mainloop()
+
 
 
 
